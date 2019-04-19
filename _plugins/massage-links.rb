da8a55438ece3d79078dc1174ec9905d95a305e0
Eric Eggert <mail@yatil.net>
2019-04-19 18:54:16 +0200

    Fix some bugs
    Signed-off-by: Eric Eggert <mail@yatil.net>

--- _plugins/massage-links.rb
+++ _plugins/massage-links.rb
@@ -1,47 +1,54 @@
 def getPage (site, ref, lang = "en")
-  translatedpages = site.documents.find_all {|a| a.data['ref'] == "/#{ref}/"}
-  translatedpages = translatedpages.concat(site.pages.find_all {|a| a.data['ref'] == "/#{ref}/"})
+  translatedpages = site.documents.find_all {|a| a.data['ref'] == ref}
+  translatedpages = translatedpages.concat(site.pages.find_all {|a| a.data['ref'] == ref})
   return translatedpages.find {|a| a.data['lang'] == lang}
 end
 
 def transform(document, inenglishtext)
-  if document.data['lang'] == 'en'
-    inenglishtext = ""
-  else
-    inenglishtext =  ' (' + inenglishtext +')'
-  end
-  document.content.gsub!(/\[\[((?:.|\n)+?)\]\]\(\/(.+?)\/(?:#(.+?))?\)/i) do |match|
-    translatedpage = getPage document.site, Regexp.last_match[2], document.data['lang']
-    if Regexp.last_match[3].nil?
-      fragment = ''
-      # Jekyll.logger.debug "Eric’s Hook X2:", Regexp.last_match[1] + "|" + Regexp.last_match[2] + Regexp.last_match[3]
+
+  unless document.content == ""
+
+    if document.data['lang'] == 'en'
+      inenglishtext = ""
     else
-      fragment = '#' + Regexp.last_match[3]
+      inenglishtext =  ' (' + inenglishtext +')'
     end
-    if translatedpage.nil?
-      '<<' + Regexp.last_match[1] +'>>({{ "/' + Regexp.last_match[2] +'/" | relative_url }}'+ fragment+'){: hreflang="en"}' + inenglishtext
-    else
-      '<<' + translatedpage.data['title'] +'>>({{ "' + translatedpage.data['permalink'] + '" | relative_url }}'+ fragment+')'
+    document.content.gsub!(/\[\[([^\]\]]+?)\]\]\((?!\/TR)(?!\/WAI)(\/.*?\/)(?:#(.*?))?\)/i) do |match|
+      translatedpage = getPage document.site, Regexp.last_match[2], document.data['lang']
+      if Regexp.last_match[3].nil?
+        fragment = ''
+        # Jekyll.logger.debug "Eric’s Hook X2:", Regexp.last_match[1] + "|" + Regexp.last_match[2] + Regexp.last_match[3]
+      else
+        fragment = '#' + Regexp.last_match[3]
+      end
+      if translatedpage.nil?
+        '<<' + Regexp.last_match[1] +'>>({{ "' + Regexp.last_match[2] +'" | relative_url }}'+ fragment+'){: hreflang="en"}' + inenglishtext
+      else
+        '<<' + translatedpage.data['title'] +'>>({{ "' + translatedpage.data['permalink'] + '" | relative_url }}'+ fragment+')'
+      end
     end
+
+    document.content.gsub!(/\[([^\]]+?)\]\((?!\/TR)(?!\/WAI)(\/.*?\/)(?:#(.*?))?\)/i) do |match|
+      translatedpage = getPage document.site, Regexp.last_match[2], document.data['lang']
+      if Regexp.last_match[3].nil?
+        fragment = ''
+        # Jekyll.logger.debug "Eric’s Hook X2:", Regexp.last_match[1] + "|" + Regexp.last_match[2] + Regexp.last_match[3]
+      else
+        fragment = '#' + Regexp.last_match[3]
+      end
+      if translatedpage.nil?
+        '[' + Regexp.last_match[1] +']({{ "' + Regexp.last_match[2] +'" | relative_url }}'+ fragment+'){: hreflang="en"}' + inenglishtext
+      else
+        '[' + Regexp.last_match[1] +']({{ "' + translatedpage.data['permalink'] + '" | relative_url }}'+ fragment+')'
+      end
+    end
+
+    document.content.gsub!(/<<([^>>]+?)>>/i) do |match|
+        '[' + Regexp.last_match[1] +']'
+    end
+
   end
 
-  document.content.gsub!(/\[((?:.|\n)+?)\]\(\/(.+?)\/(?:#(.+))?\)/i) do |match|
-    translatedpage = getPage document.site, Regexp.last_match[2], document.data['lang']
-    if Regexp.last_match[3].nil?
-      fragment = ''
-      # Jekyll.logger.debug "Eric’s Hook X2:", Regexp.last_match[1] + "|" + Regexp.last_match[2] + Regexp.last_match[3]
-    else
-      fragment = '#' + Regexp.last_match[3]
-    end
-    if translatedpage.nil?
-      '[' + Regexp.last_match[1] +']({{ "/' + Regexp.last_match[2] +'/" | relative_url }}'+ fragment+'){: hreflang="en"}' + inenglishtext
-    else
-      '[' + Regexp.last_match[1] +']({{ "' + translatedpage.data['permalink'] + '" | relative_url }}'+ fragment+')'
-    end
-  end
-  document.content.gsub!(/<<((?:.|\n)+?)>>/i) do |match|
-      '[' + Regexp.last_match[1] +']'
-  end
 end
 
 Jekyll::Hooks.register :documents, :pre_render do |document|
