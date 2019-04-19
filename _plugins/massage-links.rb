a5b8340661fcde7f333336a2bf7ac09024f8ef4f
Eric Eggert <mail@yatil.net>
2019-04-19 12:01:21 +0200

    Don’t show (in English) on english pages, fragment identifier support
    Signed-off-by: Eric Eggert <mail@yatil.net>

--- _plugins/massage-links.rb
+++ _plugins/massage-links.rb
@@ -4,35 +4,64 @@ def getPage (site, ref, lang = "en")
   return translatedpages.find {|a| a.data['lang'] == lang}
 end
 
-def transform(document)
-  document.content.gsub!(/\[\[(.*?)\]\]\(\/(.*?)\/\)/i) do |match|
+def transform(document, inenglishtext)
+  if document.data['lang'] == 'en'
+    inenglishtext = ""
+  else
+    inenglishtext =  ' (' + inenglishtext +')'
+  end
+  document.content.gsub!(/\[\[((?:.|\n)+?)\]\]\(\/(.+?)\/(?:#(.+?))?\)/i) do |match|
     translatedpage = getPage document.site, Regexp.last_match[2], document.data['lang']
-    #Jekyll.logger.debug "Eric’s Hook X2:", translatedpage.data['title']
-    if translatedpage.nil?
-      '<<' + Regexp.last_match[1] +'>>({{ "/' + Regexp.last_match[2] +'/" | relative_url }})'
+    if Regexp.last_match[3].nil?
+      fragment = ''
+      # Jekyll.logger.debug "Eric’s Hook X2:", Regexp.last_match[1] + "|" + Regexp.last_match[2] + Regexp.last_match[3]
     else
-      '<<' + translatedpage.data['title'] +'>>({{ "' + translatedpage.data['permalink'] + '" | relative_url }})'
+      fragment = '#' + Regexp.last_match[3]
+    end
+    if translatedpage.nil?
+      '<<' + Regexp.last_match[1] +'>>({{ "/' + Regexp.last_match[2] +'/" | relative_url }}'+ fragment+'){: hreflang="en"}' + inenglishtext
+    else
+      '<<' + translatedpage.data['title'] +'>>({{ "' + translatedpage.data['permalink'] + '" | relative_url }}'+ fragment+')'
     end
   end
-  document.content.gsub!(/\[(.*?)\]\(\/(.*?)\/\)/i) do |match|
+
+  document.content.gsub!(/\[((?:.|\n)+?)\]\(\/(.+?)\/(?:#(.+))?\)/i) do |match|
     translatedpage = getPage document.site, Regexp.last_match[2], document.data['lang']
-    #Jekyll.logger.debug "Eric’s Hook X2:", translatedpage.data['title']
-    if translatedpage.nil?
-      '[' + Regexp.last_match[1] +']({{ "/' + Regexp.last_match[2] +'/" | relative_url }})'
+    if Regexp.last_match[3].nil?
+      fragment = ''
+      # Jekyll.logger.debug "Eric’s Hook X2:", Regexp.last_match[1] + "|" + Regexp.last_match[2] + Regexp.last_match[3]
     else
-      '[' + Regexp.last_match[1] +']({{ "' + translatedpage.data['permalink'] + '" | relative_url }})'
+      fragment = '#' + Regexp.last_match[3]
+    end
+    if translatedpage.nil?
+      '[' + Regexp.last_match[1] +']({{ "/' + Regexp.last_match[2] +'/" | relative_url }}'+ fragment+'){: hreflang="en"}' + inenglishtext
+    else
+      '[' + Regexp.last_match[1] +']({{ "' + translatedpage.data['permalink'] + '" | relative_url }}'+ fragment+')'
     end
   end
-  document.content.gsub!(/<<(.*?)>>/i) do |match|
+  document.content.gsub!(/<<((?:.|\n)+?)>>/i) do |match|
       '[' + Regexp.last_match[1] +']'
   end
 end
 
-
 Jekyll::Hooks.register :documents, :pre_render do |document|
-  transform document
+  inenglishtext = document.site.data['translations'].find {|a| a['en'] == 'in English'}
+  inenglishtext = inenglishtext[document.data['lang']]
+  if inenglishtext.nil?
+    inenglishtext = "in English"
+  end
+  Jekyll.logger.debug "Eric’s Hook EN:", inenglishtext
+
+  transform document, inenglishtext
 end
 
 Jekyll::Hooks.register :pages, :pre_render do |document|
-  transform document
+  inenglishtext = document.site.data['translations'].find {|a| a['en'] == 'in English'}
+  inenglishtext = inenglishtext[document.data['lang']]
+  if inenglishtext.nil?
+    inenglishtext = "in English"
+  end
+  Jekyll.logger.debug "Eric’s Hook EN:", inenglishtext
+
+  transform document, inenglishtext
 end
